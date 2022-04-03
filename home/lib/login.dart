b90c30b90365b9b0b7ee328cc0bd089ce88b7c86
Yousef AlSafadi <usf.m7md@gmail.com>
2022-04-04 01:41:37 +0300

    final edit

--- home/lib/login.dart
+++ home/lib/login.dart
@@ -15,112 +15,97 @@ class LoginPage extends StatelessWidget {
           title: const Text('Task 2 '),
           backgroundColor: Colors.blueGrey,
         ),
-        body: const MyStatefulWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Abel',
-                      fontSize: 40),
-                )),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Email or username:',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'username',
-                ),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Password',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'password',
-                ),
-              ),
-            ),
-            Container(
-                height: 70,
-                width: 50,
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                child: ElevatedButton(
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Georgia',
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Abel',
+                          fontSize: 40),
+                    )),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Email or username:',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'username',
                     ),
                   ),
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                  ),
-                )),
-            Row(
-              children: <Widget>[
-                const Text(
-                  'to create new account',
                 ),
-                TextButton(
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.deepOrangeAccent,
-                        fontWeight: FontWeight.bold),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Password',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'password',
+                    ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const SignUpPage()), //signup screen
-                    );
-                  },
-                )
+                ),
+                Container(
+                    height: 70,
+                    width: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Georgia',
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueGrey,
+                      ),
+                    )),
+                Row(
+                  children: <Widget>[
+                    const Text('to create new account'),
+                    TextButton(
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const SignUpPage()), //signup screen
+                        );
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.end,
+                ),
               ],
-              mainAxisAlignment: MainAxisAlignment.end,
-            ),
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
--- home/lib/signup.dart
+++ home/lib/signup.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:home/login.dart';
@@ -17,147 +16,134 @@ class SignUpPage extends StatelessWidget {
           title: const Text('Task 2 '),
           backgroundColor: Colors.blueGrey,
         ),
-        body: const MyStatefulWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  ' Register',
-                  style: TextStyle(
-                      fontFamily: 'Abel',
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40),
-                )),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Username',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'pick your username',
-                ),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Email',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'eg: usf.m7md@gmail.com',
-                ),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Password',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'write your password',
-                ),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: const Text(
-                  'Confirm Password',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                )),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'confirm your password',
-                ),
-              ),
-            ),
-            Container(
-                height: 70,
-                width: 50,
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                child: ElevatedButton(
-                  child: const Text(
-                    'Register NOW',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Georgia',
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      ' Register',
+                      style: TextStyle(
+                          fontFamily: 'Abel',
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40),
+                    )),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Username',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'pick your username',
                     ),
                   ),
-                  onPressed: () {
-                    print("usernameCont.text");
-                    print("passCont.text");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
+                ),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Email',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'eg: usf.m7md@gmail.com',
+                    ),
                   ),
-                )),
-            Row(
-              children: <Widget>[
-                const Text('already have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.deepOrangeAccent,
-                        fontWeight: FontWeight.bold),
+                ),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Password',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'write your password',
+                    ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const LoginPage()), //signup screen
-                    );
-                  },
-                )
+                ),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: const Text(
+                      'Confirm Password',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    )),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'confirm your password',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 70,
+                    width: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text(
+                        'Register NOW',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Georgia',
+                        ),
+                      ),
+                      onPressed: () {
+                        print("usernameCont.text");
+                        print("passCont.text");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueGrey,
+                      ),
+                    )),
+                Row(
+                  children: <Widget>[
+                    const Text('already have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const LoginPage()), //signup screen
+                        );
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.end,
+                ),
               ],
-              mainAxisAlignment: MainAxisAlignment.end,
-            ),
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
